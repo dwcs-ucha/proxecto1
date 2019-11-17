ab89ba79c7c1caf827b2429fc7fe7178eb1d4c75
BreoBeceiro <57591916+BreoArmando@users.noreply.github.com>
2019-11-17 13:14:50 +0100

    Update pe.php

--- layout/pe.php
+++ layout/pe.php
@@ -1,3 +1,20 @@
 <?php
+  //17/11/2019 | Versión 1.0
+  //PROPOSTA DUNHA ESTRUTURA PARA A CABECEIRA DO SITIO CON BootStrap 4.
 
+  <nav class="navbar navbar-expand-sm bg-info navbar-dark">
+    <a class="navbar-brand" href="#">Proxecto 1ª</a>
+
+    <ul class="navbar-nav">
+      <li class="nav-item">
+        <a class="nav-link" href="#">Inicio</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Clasificación</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Actividades</a>
+      </li>
+    </ul>
+  </nav>
 ?>
