7824212af1f7c210bb84cf0009029e4955df3220
BreoArmando <breo.beceiro@gmail.com>
2019-11-17 13:56:58 +0100

    Engadida actividade1 | Version 1.0

---
+++ actividade1/actividadeDoada_Utilidades.php
@@ -0,0 +1,7 @@
+<?php
+    // BreoBeceiro:12/11/2019
+    // PROXECTO 1º AVALIACIÓN | Versión 1.0
+
+    // HAI QUE DEFINIR UN ARRAY NO QUE SE ALMACENEN AS IMAXES DA ACTIVIDADE.
+    $silabasFinais= array("1"=>"PIZ", "2"=>"CHE", "3"=>"BRO", "4"=>"RO", "5"=>"NA");
+?>
