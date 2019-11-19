7103a37236231bc1167990cb36bd79b31808200d
Sercalod <mynameisnearn@gmail.com>
2019-11-19 20:00:33 +0100

    Engadido cartafol actividade3

---
+++ actividades/actividade3/index.php
@@ -0,0 +1,14 @@
+<?php
+/**
+* @título     Proxecto 2 - Xogo  
+* @autor      Manuel Ángel Calvo Piñeiro
+* @versión    1 
+* @modificado 14/11/2019
+*/
+?>
+
+// require_once layouts/cabeceira.php
+
+
+// require_once layouts/pe.php
+
