5c023f638cc53a5b034879e0dbace46eeb87161d
Sercalod <mynameisnearn@gmail.com>
2019-11-20 18:39:28 +0100

    Version 1.1

--- actividades/actividade3/index.php
+++ actividades/actividade3/index.php
@@ -3,12 +3,14 @@
 * @título     Proxecto 2 - Xogo  
 * @autor      Manuel Ángel Calvo Piñeiro
 * @versión    1 
-* @modificado 14/11/2019
+* @modificado 20/11/2019
 */
 ?>
+<?php
+define('__ROOT__', $_SERVER["DOCUMENT_ROOT"]);
+require_once (__ROOT__.'/layout/cabeceira.php');
+require_once (__ROOT__.'/layout/menu.php');
 
-// require_once layouts/cabeceira.php
-
-
-// require_once layouts/pe.php
+require_once (__ROOT__.'/layout/pe.php');
+?>
 
--- layout/cabeceira.php
+++ layout/cabeceira.php
@@ -1,7 +1,19 @@
 <?php
   //17/11/2019 | Versión 1.0
   //PROPOSTA DUNHA ESTRUTURA PARA A CABECEIRA DO SITIO CON BootStrap 4.
-
+  
+  //20/11/2019 | Versión 1.1
+?>
+<!doctype html>
+<html lang="gl">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+</head>
+<body>
+  <!-- MENÚ DE NAVEGACIÓN HORIZONTAL -->
   <nav class="navbar navbar-expand-sm bg-info navbar-dark">
     <a class="navbar-brand" href="#">Proxecto 1ª</a>
 
@@ -17,4 +29,4 @@
       </li>
     </ul>
   </nav>
-?>
+  <!-- CONTIDO -->
