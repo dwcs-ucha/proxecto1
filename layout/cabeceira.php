b700b6d04f300c364dff985ec2c8b068ba254510
Sercalod <45317313+Sercalod@users.noreply.github.com>
2019-11-21 18:41:26 +0100

    Update cabeceira.php

--- layout/cabeceira.php
+++ layout/cabeceira.php
@@ -4,19 +4,6 @@
   
   //20/11/2019 | Versión 1.1
 ?>
-<!doctype html>
-<html lang="gl">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-  <link rel="stylesheet" href="../../estilos/estilos.css" type="text/css">
-  <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
-  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
-</head>
-<body>
-  <!-- MENÚ DE NAVEGACIÓN HORIZONTAL -->
   <nav class="navbar navbar-expand-sm bg-info navbar-dark">
     <a class="navbar-brand" href="#">Proxecto 1ª</a>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
@@ -37,4 +24,3 @@
 	</div>
   </nav>
   <!-- CONTIDO -->
-  <div class="container" style="max-width: 1550px;">
