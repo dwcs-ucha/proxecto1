5345abb2344028e8adcda6f493b1d9e77a847980
ZerinhoRomero <zerinhoromero@gmail.com>
2019-11-28 22:09:22 +0100

    Versión 0.0.8b Bootstrap co Index modificado

--- actividades/actividade11/proxecto11_index.php
+++ actividades/actividade11/proxecto11_index.php
@@ -20,30 +20,41 @@
 		include '../../layout/cabeceira.php';
 	?>
 	<h1>Emparella imaxes</h1>
-	<div class="container-fluid corpo">
-		<div class="row">
-			<div class="col-xs-2 col-sm-2 col-md-2 col-lg-2 marxe"></div>
-			<div class="col-xs-3 col-sm-3 col-md-3 col-lg-3 imaxe">
-				<img class="img-thumbnail" src="imaxes/emparella.gif">
+	<form id="facil" action="proxecto11_xogo.php" method="post">
+		<div class="container-fluid corpo">
+			<div class="row">
+				<div class="col-xs-2 col-sm-2 col-md-2 col-lg-2 marxe"></div>
+				<div class="col-xs-3 col-sm-3 col-md-3 col-lg-3 imaxe">
+					<img class="img-thumbnail" src="imaxes/emparella.gif">
+				</div>
+				<div class="col-xs-5 col-sm-5 col-md-5 col-lg-5 resumo">
+					<span>Xogo que consiste en emparellar as cartas de froitas que están viradas boca abaixo</span>
+				</div>
+				<div class="col-xs-2 col-sm-2 col-md-2 col-lg-2 marxe"></div>
 			</div>
-			<div class="col-xs-5 col-sm-5 col-md-5 col-lg-5 resumo">
-				<span>Xogo que consiste en emparellar as cartas de froitas que están viradas boca abaixo</span>
+			<div class="row">
+				<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 dificultade" align="center">
+					<h4>Dificultade</h4>
+					<div class="btn-group btn-group-toggle" data-toggle="buttons">
+						<label class="btn btn-secondary active btn-success">
+							<input type="radio" name="numeroCartas" value="6" checked/>Fácil
+						</label>
+						<label class="btn btn-secondary active btn-warning">
+							<input type="radio" name="numeroCartas" value="12"/>Normal
+						</label>
+						<label class="btn btn-secondary active btn-danger">
+						<input type="radio" name="numeroCartas" value="18"/>Difícil
+						</label>
+					</div>
+				</div>
 			</div>
-			<div class="col-xs-2 col-sm-2 col-md-2 col-lg-2 marxe"></div>
+			<div class="row">
+				<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 xogar" align="center">
+					 <button class="btn btn-lg btn-success" type="submit" name="enviar" value="enviar">Xogar</button>
+				</div>
+			</div>
 		</div>
-		<form id="facil" action="proxecto11_xogo.php" method="post">
-			<input type="submit" id="facil" name="facil" value="Nivel Fácil">
-			<input type="hidden" name="numeroCartas" value="6">
-		</form>
-		<form id="medio" action="proxecto11_xogo.php" method="post">
-			<input type="submit" id="medio" name="medio" value="Nivel Medio">
-			<input type="hidden" name="numeroCartas" value="12">
-		</form>
-		<form id="dificil" action="proxecto11_xogo.php" method="post">
-			<input type="submit" id="dificil" name="dificil" value="Nivel Difícil">
-			<input type="hidden" name="numeroCartas" value="18">
-		</form>
-	</div>
+	</form>
 	<?php
 		include '../../layout/pe.php';
 	?>
