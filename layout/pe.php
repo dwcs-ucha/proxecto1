4ba7bede5a1ec59e5e4e3a9a4037e444579de730
BreoBeceiro <57591916+BreoArmando@users.noreply.github.com>
2019-11-28 22:01:46 +0100

    Actualizado o enlace e a mensaxe da licencia.

--- layout/pe.php
+++ layout/pe.php
@@ -3,7 +3,7 @@
 
     <!-- Copyright -->
     <div class="footer-copyright text-center py-3"> 
-      <a rel="license" href="http://creativecommons.org/licenses/by/4.0/"><img alt="Licencia de Creative Commons" style="border-width:0; width: 120px;" class="logoCC" src="https://i.creativecommons.org/l/by/4.0/88x31.png" /></a><br />Esta obra está baixo unha <a rel="license" href="http://creativecommons.org/licenses/by/4.0/">licencia de Creative Commons Reconocimiento 4.0 Internacional</a>.
+      <a rel="license" href="http://creativecommons.org/licenses/by-nc-sa/4.0/"><img alt="Licencia de Creative Commons" style="border-width:0; width: 120px;" src="https://i.creativecommons.org/l/by-nc-sa/4.0/88x31.png" /></a><br />Este obra está baixo unha <a rel="license" href="http://creativecommons.org/licenses/by-nc-sa/4.0/">licencia de Creative Commons Reconocimiento-NoComercial-CompartirIgual 4.0 Internacional</a>
       <br />
       <a href="https://www.cifprodolfoucha.es/" target="_blank" class="copyright-link" title="Centro educativo"> C.I.F.P. Rodolfo Ucha Piñeiro </a><span class="copyright"> © 2019 Copyright: </span>
     </div>
