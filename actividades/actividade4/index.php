14cfce8f0259791e420080b1bf4ef17d5379b842
Santiago <Santiago@Santiago-PC>
2019-11-29 22:29:39 +0100

    Inclúe a nova forma de incorporar a cabeceira

--- actividades/actividade4/index.php
+++ actividades/actividade4/index.php
@@ -1,68 +1,61 @@
 <!DOCTYPE html>
 <html>
-    <head>
-        <?php
-	//Autor: Santiago Calvo Piñeiro
-        require_once '../../layout/head.php'; /* Contén etiquetas coma meta, link, script */
-        ?>
-        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-        <title>Agrupar elementos</title>
-        <style>
-            .btn-lg {
-                padding:25px 100px;
-            }
-            .btn-secondary {
-                padding: 0px 30px;
-            }
-        </style>
-    </head>
-    <body>
-        <?php
-        require_once '../../layout/cabeceira.php'; /* Contén a cabeceira, que consiste nun menú horizontal <nav> [...] </nav> */
-        ?>
-        <form action="actividade.php" method="post" class="container-fluid">
-            <div class="row">
-                <div class="col-1"></div>
-                <div class="col-3">
-                    <img src="icono.png" height="300" width="300"/>
-                </div>
-                <div class="col-5">
-                    <h1>Xogo de agrupar elementos por categorías</h1>
-                    <p>
-                        Material para traballar a pertenencia ou non pertenencia dun obxeto a unha categoría de vocabulario dada.
-                    </p>
-                </div>
-		<div class="col-1"></div>
-            </div>
-            <div class="row">
-                <div class="col-4"></div>
-                <div class="col-7">    
-                    <h4>Dificultade</h4>
-                    <div class="btn-group btn-group-toggle" data-toggle="buttons">
-                        <label class="btn btn-secondary active btn-success">
-                            <input type="radio" name="dificultade" value="facil"/>Fácil
-                        </label>
-                        <label class="btn btn-secondary active btn-warning">
-                            <input type="radio" name="dificultade" value="normal" checked/>Normal
-                        </label>
-                        <label class="btn btn-secondary active btn-danger">
-                            <input type="radio" name="dificultade" value="dificil"/>Difícil
-                        </label>
-                    </div>
-                </div>
-                <div class="col-1"></div>
-            </div>
-            <br/>
-            <div class="row">
-                <div class="col-4"></div>
-                <div class="col-7">
-                    <button class="btn btn-lg btn-success" type="submit" name="enviar" value="enviar">Xogar</button>
-                </div>
-                <div class="col-1"></div>
-            </div>
-        </form>
-        <?php
-        require_once '../../layout/pe.php'; /* Contén o pé da páxina (<footer>[...]</footer>) */
-        ?>
-    </body>
+<head>
+	<?php
+		/**
+		* @Autor: Santiago Calvo Piñeiro
+		**/
+		include '../../layout/head.php';
+                $directorioRaiz ="../..";
+                escribirHead();
+	?>
+	<meta charset="utf-8">
+	<title>Agrupar elementos</title>
+</head>
+<body>
+	<?php
+		include '../../layout/cabeceira.php';
+                escribirCabeceira();
+	?>
+	<h1>Xogo de agrupar elementos por categorías</h1>
+        <form id="facil" action="actividade.php" method="post">
+		<div class="container-fluid corpo">
+			<div class="row">
+				<div class="col-xs-2 col-sm-2 col-md-2 col-lg-2 marxe"></div>
+				<div class="col-xs-3 col-sm-3 col-md-3 col-lg-3 imaxe">
+					<img src="icono.png" height="300" width="300"/>
+				</div>
+				<div class="col-xs-5 col-sm-5 col-md-5 col-lg-5 resumo">
+					<span>Material para traballar a pertenencia ou non pertenencia dun obxeto a unha categoría de vocabulario dada.</span>
+				</div>
+				<div class="col-xs-2 col-sm-2 col-md-2 col-lg-2 marxe"></div>
+			</div>
+			<div class="row">
+				<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 dificultade" align="center">
+					<h4>Dificultade</h4>
+					<div class="btn-group btn-group-toggle" data-toggle="buttons">
+						<label class="btn btn-secondary active btn-success">
+							<input type="radio" name="dificultade" value="facil"/>Fácil
+						</label>
+						<label class="btn btn-secondary active btn-warning">
+							<input type="radio" name="dificultade" value="normal"/>Normal
+						</label>
+						<label class="btn btn-secondary active btn-danger">
+							<input type="radio" name="dificultade" value="dificil"/>Difícil
+						</label>
+					</div>
+				</div>
+			</div>
+			<div class="row">
+				<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 xogar" align="center">
+                                         <br/>
+					 <button class="btn btn-lg btn-success" type="submit" name="enviar" value="enviar">Xogar</button>
+				</div>
+			</div>
+		</div>
+	</form>
+	<?php
+		include '../../layout/pe.php';
+	?>
+</body>
 </html>
