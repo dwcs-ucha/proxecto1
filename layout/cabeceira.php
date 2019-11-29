1baf4e01822d2d704f3f25960f3a65e1ea5ce6c9
Santiago <Santiago@Santiago-PC>
2019-11-29 16:12:54 +0100

    Nova forma de incluir a cabeceira e o head nas páxinas. Pode consultarse o seu uso aquí https://github.com/dwcs-ucha/proxecto1/issues/9

--- layout/cabeceira.php
+++ layout/cabeceira.php
@@ -1,23 +1,32 @@
 <?php
-  //27/11/2019 | Versión 1.1
-  //PROPOSTA DUNHA ESTRUTURA PARA A CABECEIRA DO SITIO CON BootStrap 4.
+//27/11/2019 | Versión 1.1
+//PROPOSTA DUNHA ESTRUTURA PARA A CABECEIRA DO SITIO CON BootStrap 4.
 ?>
-  <nav class="navbar navbar-expand-sm bg-primary navbar-dark">
-    <a class="navbar-brand" href="../../index.php">Proxecto 1ª</a>
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-	</button>
-	<div class="collapse navbar-collapse" id="navbarSupportedContent">
-	  <ul class="navbar-nav" mr-auto>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Categoría 1</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Categoría 2</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Categoría 3</a>
-        </li>
-      </ul>
-	</div>
-  </nav>
+<?php
+$directorioRaiz = "";
+
+function escribirCabeceira() {
+    global $directorioRaiz;
+    ?>
+    <nav class="navbar navbar-expand-sm bg-primary navbar-dark">
+        <a class="navbar-brand" href="<?=$directorioRaiz?>/index.php">Proxecto 1ª</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav" mr-auto>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Categoría 1</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Categoría 2</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Categoría 3</a>
+                </li>
+            </ul>
+        </div>
+    </nav>
+<?php }
+?>
+
