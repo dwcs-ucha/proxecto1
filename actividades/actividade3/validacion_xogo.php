6552d2386e83dc010bd3a6ea662e7720d654aeb0
Sercalod <mynameisnearn@gmail.com>
2019-11-30 20:22:53 +0100

    Actividade 3

---
+++ actividades/actividade3/validacion_xogo.php
@@ -0,0 +1,22 @@
+<?php
+$minDificultade = 1;
+$maxDificultade = 3;
+$dificultade = $minDificultade; //dificultade por defecto
+
+function validarDificultade($difPost){
+ global $minDificultade;
+ global $maxDificultade;
+ if(filter_var($difPost, FILTER_VALIDATE_INT, array("options" => array("min_range"=>$minDificultade, "max_range"=>$maxDificultade))) === false) {
+  return false;
+ } else {
+  return true;
+ }
+}
+
+if(!empty($_POST['dificultade'])){
+ if(validarDificultade($_POST['dificultade'])){
+  $dificultade = $_POST['dificultade'];
+ }
+}
+var_dump($dificultade);
+?>
