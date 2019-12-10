b6a9f1b7d76fe4176e54b14919086587d69885f0
javiertg222 <58482162+javiertg222@users.noreply.github.com>
2019-12-10 20:54:06 +0100

    Update sonidos.php

--- actividades/actividad5/sonidos.php
+++ actividades/actividad5/sonidos.php
@@ -2,22 +2,30 @@
 <html>
 <head>
 <title>Juego de los Sonidos</title>
-<link rel="stylesheet" href="/proxecto1/actividades/actividad5/css/sonidos.css">
+<link rel="stylesheet" href="css/sonidos.css">
+<?php
+/**
+         * @Autor: Javier Torrón González
+         * */
+     
+        include '../../layout/head.php';
+?>
 </head>
 <body>
 <?php
+include '../../layout/cabeceira.php';
 $animales=["perro", "vaca", "mono", "cerdo", "leon", "gallo", "burro", "cabra", "elefante", "pajaro", "gato", "caballo"];
 $aleatorio=rand(0,11);
 ?>
 <div id="sonidos">
 <h3>Presta atención!!!</h3>
-<audio controls src="/proxecto1/actividades/actividad5/mp3/<?php echo $animales[$aleatorio]; ?>.mp3" type="audio/mp3"></audio>
+<audio controls src="mp3/<?php echo $animales[$aleatorio]; ?>.mp3" type="audio/mp3"></audio>
 <br><br>
 <?php
 foreach($animales as $key=>$valor){
 
 ?>
-<a href="sonidos.php?animal=<?php echo $key; ?>&random=<?php echo $aleatorio; ?>"><img id="son" src="/proxecto1/actividades/actividad5/imagenes/<?php echo $valor; ?>.png" alt="<?php echo $valor; ?>"></a>
+<a href="sonidos.php?animal=<?php echo $key; ?>&random=<?php echo $aleatorio; ?>"><img id="son" src="imagenes/<?php echo $valor; ?>.png" alt="<?php echo $valor; ?>"></a>
 
 <?php } ?>
  <br>
@@ -45,5 +53,8 @@ if ($_SERVER["REQUEST_METHOD"] == "GET"){
 
 ?>
 </div>
+<?php
+include '../../layout/pe.php';
+?>
 </body>
 </html>
