0a4d2e4dcc1128492edaa8cd9fd7d53765c547bd
javiertg222 <58482162+javiertg222@users.noreply.github.com>
2019-12-10 20:09:59 +0100

    Update index.php

--- actividades/actividad5/index.php
+++ actividades/actividad5/index.php
@@ -3,29 +3,29 @@
 <head>
 <title>Jugando con sonidos</title>
 <link rel="stylesheet" href="css/sonidos.css">
- <?php
+<?php
  /**
          * @Autor: Javier Torrón González
          * */
-        $directorioRaiz = "../..";
+     
         include '../../layout/head.php';
         ?>
 </head>
 <body>
- <?php
+<?php
  include '../../layout/cabeceira.php';
 ?>
 <div id="inicio">
 <h1>Bienvenido a juega con sonidos.</h1>
 <p>El juego trata de relacionar un sonido con su animal. Pincha en el play y cuando lo tengas claro pulsa el icono del animal.</p>
-<img id="ini" src="/proxecto1/actividades/actividad5/imagenes/delfin.gif"/>
+<img id="ini" src="imagenes/delfin.gif"/>
 <form action="sonidos.php" method="GET">
   <p>Suerte!!!. Comencemos.</p>
   <br><br>
   <input class="boton" type="submit" value="Jugar">
 </form>
 </div>
- <?php
+<?php
  include '../../layout/pe.php';
 ?>
 </body>
