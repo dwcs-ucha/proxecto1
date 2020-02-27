6dd4e2ea7e33cd8e04907c22c6676df55b6f7534
“BreoArmando” <breo.beceiro@gmail.com>
2020-02-27 20:08:40 +0100

    Traballando no Modelo...

--- actividades/actividade1/Modelo/DAO.class.php
+++ actividades/actividade1/Modelo/DAO.class.php
@@ -1,15 +1,13 @@
 <?php
-    // BreoBeceiro:20/02/2020
+    // BreoBeceiro:27/02/2020
     // PROXECTO 2ª AVALIACIÓN | Versión 1.0
 
-    // OS MÉTODOS obterProduto() E obterProdutos() NON TEÑEN CABIDA NESTE PROXECTO, POIS A APLICACIÓN É 'Xogoteca',
-    //   SÓ HAI XOGADORES E PARTIDAS.
     // FALTA UN MÉTODO gardarPuntuacion($id) QUE FAGA UN insert NA TÁBOA PARTIDAS PARA ESE USUARIO COA PUNTUACIÓN
     //   QUE OBTIVO.
 
     class DAO{
 
-        // Conecta coa BBDD 'actividade1_BBDD' por medio do usuario 'breogan'.
+        // Conecta coa BBDD 'actividade1_BBDD' por medio do usuario 'breo'.
         // Devolve o obxecto PDO da conexión coa BBDD en caso de éxito, ou unha mensaxe de erro, en caso contrario.
         public static function conectaBBDD(){
             try{
@@ -29,66 +27,55 @@
         }
 
         // Recibe un nome de usuario e o seu contrasinal. 
-        // Devolve o obxecto Usuario se existen os campos no CSV e FALSE, se non.
-        public static function comprobaUsuario($usuario, $contrasinal){
-            // SELECT usuario, contrasinal FROM actividade1_BBDD WHERE usuario=:usuario AND contrasinal=.contrasinal;
-            // Compróbase a coincidencia de ambos campos e se devolvce o obxecto Xogador.
-            return true;
-        }
-
-        // Parámetro: O código do produto a obter.
-        // Devolve: Un obxecto PDO
-        public static function obterProduto($cod){
+        // Devolve TRUE se o obxecto Usuario existe e FALSE, se non.
+        public static function comprobaXogador($xogador, $contrasinal){
             try{
                 $conexion= DAO::conectaBBDD();
 
-                $consulta= $conexion->prepare('SELECT * FROM produtos WHERE codigo=:codigo');
-                $consulta->bindParam(":codigo", $cod);
-                $consulta->execute();
+                $consulta= $conexion->prepare('SELECT usuario, contrasinal FROM actividade1_BBDD WHERE usuario=:usuario AND contrasinal=:contrasinal');
+                $consulta->bindParam(":usuario", $xogador);
+                $consulta->bindParam(":contrasinal", $contrasinal);
 
-                $resultado= $consulta->rowCount();
+                if($consulta->execute()){
+                    $resultado= true;
+                }else{
+                    $resultado= false;
+                }
 
             }catch(PDOException $e){
-                $conexion= "Erro conectando coa base de datos ". $e->getMessage();
+                $conexion= "Erro na conexión coa BBDD: ". $e->getMessage();
 
             }finally{
-                if($resultado==1){
-                    return $resultado;
-                }else{
-                    return false;
-                }
+                return $resultado;
+
             }
         }
 
-        // Devolve: Un obxecto 
-        public static function obterUsuario($cod){
+        // Devolve: Un obxecto Xogador en caso de éxito ou unha mensaxe de erro, en caso contrario.
+        public static function obterXogador($cod){
             try{
                 $conexion= DAO::conectaBBDD();
 
-                $consulta= $conexion->prepare('SELECT * FROM usuarios WHERE codigo=:codigo');
+                $consulta= $conexion->prepare('SELECT * FROM a1_xogador WHERE codigo=:codigo');
                 $consulta->bindParam(":codigo", $cod);
                 $consulta->execute();
 
-                $resultado= $consulta->rowCount();
+                $xogador= $resultado->fetch(PDO::FETCH_OBJ);
 
             }catch(PDOException $e){
-                $conexion= "Erro conectando coa base de datos ". $e->getMessage();
+                $xogador= "Erro conectando coa base de datos ". $e->getMessage();
 
             }finally{
-                if($resultado==1){
-                    return $resultado;
-                }else{
-                    return false;
-                }
+                return $xogador;
             }
         }
 
         // Devolve: Un obxecto PDO 
-        public static function obterProdutos(){
+        public static function obterXogadores(){
             try{
                 $conexion= DAO::conectaBBDD();
 
-                $consulta= $conexion->prepare('SELECT * FROM produtos');
+                $consulta= $conexion->prepare('SELECT * FROM a1_xogadores');
                 $consulta->execute();
 
                 $resultado= $consulta->rowCount();
@@ -105,28 +92,11 @@
             }
         }
 
-        // Devolve: Un obxecto PDO
-        public static function obterUsuarios(){
-            try{
-                $conexion= DAO::conectaBBDD();
-
-                $consulta= $conexion->prepare('SELECT * FROM usuarios');
-                $consulta->execute();
-                
-                $resultado= $consulta->rowCount();
-
-            }catch(PDOException $e){
-                $conexion= "Erro conectando coa base de datos ". $e->getMessage();
-
-            }finally{
-                if(isset($resultado)){
-                    if($resultado>=1){
-                        return $resultado;
-                    }
-                }else{
-                    return false;
-                }
-            }
+        // Fai un INSERT na táboa a1_partidas para a tupla na que o campo 'codigo' coincida co parámetro recibido.
+        // Devolve TRUE en caso de éxito e FALSE, en caso de erro.
+        public static function gardarPuntuacion($id){
+            // Código para gardar os puntos do xogador...
+            return true;
         }
 
     }
