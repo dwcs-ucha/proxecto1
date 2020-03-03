c03e8bb9e2f41bad7e892ed73edcee44414b6baf
SdeScotty <34619625+SdeScotty@users.noreply.github.com>
2020-03-03 18:41:14 +0100

    Engadido script BBDD

---
+++ xogoteca.sql
@@ -0,0 +1,72 @@
+-- phpMyAdmin SQL Dump
+-- version 4.8.5
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: localhost
+-- Tiempo de generación: 03-03-2020 a las 18:39:07
+-- Versión del servidor: 5.7.29-0ubuntu0.18.04.1
+-- Versión de PHP: 7.2.24-0ubuntu0.18.04.3
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `xogoteca`
+--
+CREATE DATABASE IF NOT EXISTS `xogoteca` DEFAULT CHARACTER SET latin1 COLLATE latin1_general_ci;
+USE `xogoteca`;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `estadisticas`
+--
+
+CREATE TABLE `estadisticas` (
+  `codactividade` varchar(4) COLLATE latin1_general_ci NOT NULL,
+  `nomexogador` varchar(10) COLLATE latin1_general_ci NOT NULL,
+  `data` datetime NOT NULL,
+  `puntos` int(11) NOT NULL,
+  `dificultade` enum('baixa','media','dificil','') COLLATE latin1_general_ci DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `usuarios`
+--
+
+CREATE TABLE `usuarios` (
+  `nome` varchar(10) COLLATE latin1_general_ci NOT NULL,
+  `contrasinal` varchar(10) COLLATE latin1_general_ci NOT NULL,
+  `rol` tinyint(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_general_ci;
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `estadisticas`
+--
+ALTER TABLE `estadisticas`
+  ADD PRIMARY KEY (`codactividade`,`nomexogador`,`data`);
+
+--
+-- Indices de la tabla `usuarios`
+--
+ALTER TABLE `usuarios`
+  ADD PRIMARY KEY (`nome`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
