cfdeb00dd6364741c74b6aee88aad304971ec5e5
Sercalod <45317313+Sercalod@users.noreply.github.com>
2020-03-03 18:55:14 +0100

    Update xogoteca.sql

--- xogoteca.sql
+++ xogoteca.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: localhost
--- Tiempo de generación: 03-03-2020 a las 18:39:07
+-- Tiempo de generación: 03-03-2020 a las 18:54:37
 -- Versión del servidor: 5.7.29-0ubuntu0.18.04.1
 -- Versión de PHP: 7.2.24-0ubuntu0.18.04.3
 
@@ -35,7 +35,7 @@ CREATE TABLE `estadisticas` (
   `nomexogador` varchar(10) COLLATE latin1_general_ci NOT NULL,
   `data` datetime NOT NULL,
   `puntos` int(11) NOT NULL,
-  `dificultade` enum('baixa','media','dificil','') COLLATE latin1_general_ci DEFAULT NULL
+  `dificultade` enum('baixa','media','dificil') COLLATE latin1_general_ci DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_general_ci;
 
 -- --------------------------------------------------------
@@ -58,13 +58,24 @@ CREATE TABLE `usuarios` (
 -- Indices de la tabla `estadisticas`
 --
 ALTER TABLE `estadisticas`
-  ADD PRIMARY KEY (`codactividade`,`nomexogador`,`data`);
+  ADD PRIMARY KEY (`codactividade`,`nomexogador`,`data`),
+  ADD KEY `nomexogador` (`nomexogador`);
 
 --
 -- Indices de la tabla `usuarios`
 --
 ALTER TABLE `usuarios`
   ADD PRIMARY KEY (`nome`);
+
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `estadisticas`
+--
+ALTER TABLE `estadisticas`
+  ADD CONSTRAINT `estadisticas_ibfk_1` FOREIGN KEY (`nomexogador`) REFERENCES `usuarios` (`nome`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
