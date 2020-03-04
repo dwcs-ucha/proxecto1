20d0a4f7934cf9217bb307ccf408ad5407867bed
BreoBeceiro <57591916+BreoArmando@users.noreply.github.com>
2020-03-04 20:10:42 +0100

    Traballando en 'Validacion.class.php'

--- actividades/actividade1/Modelo/Validacion.class.php
+++ actividades/actividade1/Modelo/Validacion.class.php
@@ -1,8 +1,9 @@
 <?php
-    // BreoBeceiro:03/03/2020
+    // BreoBeceiro:04/03/2020
     // PROXECTO 2ª AVALIACIÓN | Versión 1.0
 
-    // ESTA CLASE SERÁ POSTA A DISPOSICIÓN DE TODO O GRUPO.
+    // OS MÉTODOS ESPECIALMENTE DESENVOLVIDOS PARA O SEU USO NALGUNHA ACTIVIDADE, TEÑEN UN COMENTARIO PREVIO NO QUE SE PODE 
+    //   AVERIGUAR PARA QUE ACTIVIDADE FOI IMPLANTADO, ASÍ COMO O COLABORADOR AO CARGO DE DITA ACTIVIDADE.
 
     class Validacion{
 
@@ -32,6 +33,113 @@
                     $resultado= true;
                 }else{
                     $resultado= false;
+                }
+            }else{
+                $resultado= false;
+            }
+
+            return $resultado;
+        }
+
+        // MÉTODO DE VALIDACIÓN PARA A DIFICULTADE BAIXA DA 'actividade6' (Luis Corral de Cal).
+        // Recibe un número enteiro e devolve TRUE soamente se está dentro do rango do 0 (incluído) ata o 100 (sen incluír).
+        public static function a6_validaNumero_facil($numero){
+            if(is_int($numero)){
+                if($numero<0){
+                    $resultado= false;
+                }elseif($numero>=100){
+                    $resultado= false;
+                }else{
+                    if(Validacion::validaInt($numero)){
+                        $resultado= true;
+                    }else{
+                        $resultado= false;
+                    }
+                }
+            }else{
+                $resultado= false;
+            }
+
+            return $resultado;
+        }
+
+        // MÉTODO DE VALIDACIÓN PARA A DIFICULTADE MEDIA DA 'actividade6' (Luis Corral de Cal).
+        // Recibe un número enteiro e devolve TRUE soamente se está dentro do rango do 0 (incluído) ata o 1000 (sen incluír).
+        public static function a6_validaNumero_medio($numero){
+            if(is_int($numero)){
+                if($numero<0){
+                    $resultado= false;
+                }elseif($numero>=1000){
+                    $resultado= false;
+                }else{
+                    if(Validacion::validaInt($numero)){
+                        $resultado= true;
+                    }else{
+                        $resultado= false;
+                    }
+                }
+            }else{
+                $resultado= false;
+            }
+
+            return $resultado;
+        }
+
+        // MÉTODO DE VALIDACIÓN PARA A DIFICULTADE ALTA DA 'actividade6' (Luis Corral de Cal).
+        // Recibe un número enteiro e devolve TRUE soamente se está dentro do rango do 0 (incluído) ata o 10000 (sen incluír).
+        public static function a6_validaNumero_dificil($numero){
+            if(is_int($numero)){
+                if($numero<0){
+                    $resultado= false;
+                }elseif($numero>=10000){
+                    $resultado= false;
+                }else{
+                    if(Validacion::validaInt($numero)){
+                        $resultado= true;
+                    }else{
+                        $resultado= false;
+                    }
+                }
+            }else{
+                $resultado= false;
+            }
+
+            return $resultado;
+        }
+
+        // MÉTODO DE VALIDACIÓN PARA A DIFICULTADE X DA 'actividade4' (Santiago Calvo Piñeiro).
+        // Recibe un número enteiro e devolve TRUE soamente se está dentro do rango do 5 (incluído) ao 10 (sen incluír).
+        public static function a4_validaNumero($numero){
+            if(is_int($numero)){
+                if($numero<5){
+                    $resultado= false;
+                }elseif($numero>10){
+                    $resultado= false;
+                }else{
+                    if(Validacion::validaInt($numero)){
+                        $resultado= true;
+                    }else{
+                        $resultado= false;
+                    }
+                }
+            }else{
+                $resultado= false;
+            }
+
+            return $resultado;
+        }
+
+        // MÉTODO DE VALIDACIÓN PARA A DIFICULTADE X DA 'actividade4' (Santiago Calvo Piñeiro).
+        // Recibe un nome que, se ten menos de 30 caracteres e carece de números, producirá unha resposta positiva no
+        //   método, é dicir, TRUE. En calquer outro caso, devolverá FALSE.
+        public static function a4_validaNomeCategoria($nomeCategoria){
+            if(is_string($nomeCategoria)){
+                if(strlen($nomeCategoria)<30){
+                    if(filter_var($nomeCategoria, FILTER_SANITIZE_STRING)){
+                        $resultado= true;
+                    }else{
+                        $resultado= false;
+                    }
                 }
             }else{
                 $resultado= false;
