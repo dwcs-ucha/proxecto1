7077b8b3d6e07c6feb757cd1ecb38f20a05a1abd
AzaOtero <57678730+AzaOtero@users.noreply.github.com>
2020-03-05 19:51:49 +0100

    Añadida captura de excepciones

--- Modelo/DAO.class.php
+++ Modelo/DAO.class.php
@@ -6,6 +6,7 @@
  */
 
 require "Config.class.php";//Se meten los datos para la conexión a la base de datos
+require "Log.class.php";//Se meten los datos para escribir los errores en un log personalizado
 
  class DAO {
     
@@ -31,15 +32,22 @@ require "Config.class.php";//Se meten los datos para la conexión a la base de d
      * Descripción: Lee algunos datos de la tabla especificada
      */
     public static function leerDatos($tabla, $campos) {
-        $conexion = self::establecerConexion();//Se ejecuta la función "establecerConexion()" a partir de la clase "DAO" y se recoge su valor en la variable "$conexion"
-        self::configurarPDO($conexion);//Se ejecuta la función configurarPDO() con el parámetro "$conexion"
-        $total_campos = self::concatenarDatos($campos);//Se ejecuta la función "concatenarDatos()" con el parámetro "$campos"
-        $sentencia = "SELECT " . $total_campos . " FROM " . $tabla;//Sentencia SQL a ejecutar
+        try {//Se prueban los datos siguientes:
+            $conexion = self::establecerConexion();//Se ejecuta la función "establecerConexion()" a partir de la clase "DAO" y se recoge su valor en la variable "$conexion"
+            self::configurarPDO($conexion);//Se ejecuta la función configurarPDO() con el parámetro "$conexion"
+            $total_campos = self::concatenarDatos($campos);//Se ejecuta la función "concatenarDatos()" con el parámetro "$campos"
+            $sentencia = "SELECT " . $total_campos . " FROM " . $tabla;//Sentencia SQL a ejecutar
 
-        $resultado = $conexion->query($sentencia);//Se ejecuta esa instrucción SQL para la base de datos y se recoge el resultado en esta variable
+            $resultado = $conexion->query($sentencia);//Se ejecuta esa instrucción SQL para la base de datos y se recoge el resultado en esta variable
 
-        $lista = $resultado->fetchAll();//Se recogen todos los datos en esta variable
+            $lista = $resultado->fetchAll();//Se recogen todos los datos en esta variable
+        } catch (PDOException $e) {//Si salta un error del tipo "PDOException":
+            $log = new Log();//Se crea una instancia de la clase "Log"
+            $mensaje_error = $e->getMessage();//Mensaje de error
 
+            $log->escribeLog($mensaje_error);//Se ejecuta la función "escribeLog()" a partir del objeto "$log" con el parámetro "$mensaje_error"
+        }
+        
         return $lista;//Se devuelve la lista con los datos específicos de la tabla específica
     }
 
@@ -48,14 +56,21 @@ require "Config.class.php";//Se meten los datos para la conexión a la base de d
      * Descripción: Escribe el dato a partir de la información contenida en los arrays "$campos" y "$valores"
      */
     public static function escribirDatos($tabla, $campos, $valores) {
-        $conexion = self::establecerConexion();//Se ejecuta la función "establecerConexion()" a partir de la clase "DAO" y se recoge su valor en la variable "$conexion"
-        self::configurarPDO($conexion);//Se ejecuta la función configurarPDO() con el parámetro "$conexion"
-        $total_campos = self::concatenarDatos($campos);//Se ejecuta la función "concatenarDatos()" con el parámetro "$campos"
-        $total_valores = self::concatenarInterrogaciones($campos);//Se ejecuta la función "concatenarInterrogaciones()" con el parámetro "$campos"
+        try {//Se prueban los datos siguientes:
+            $conexion = self::establecerConexion();//Se ejecuta la función "establecerConexion()" a partir de la clase "DAO" y se recoge su valor en la variable "$conexion"
+            self::configurarPDO($conexion);//Se ejecuta la función configurarPDO() con el parámetro "$conexion"
+            $total_campos = self::concatenarDatos($campos);//Se ejecuta la función "concatenarDatos()" con el parámetro "$campos"
+            $total_valores = self::concatenarInterrogaciones($campos);//Se ejecuta la función "concatenarInterrogaciones()" con el parámetro "$campos"
 
-        $sentencia = "INSERT INTO " . $tabla . "(" . $total_campos . ") VALUES (" . $total_valores . ")";//Sentencia SQL a preparar
-        $consulta = $conexion->prepare($sentencia);//Se prepara esa sentencia SQL y se recoge su valor en esta variable
-        $consulta->execute($valores);//Se ejecuta la sentencia con los datos del array "$valores"
+            $sentencia = "INSERT INTO " . $tabla . "(" . $total_campos . ") VALUES (" . $total_valores . ")";//Sentencia SQL a preparar
+            $consulta = $conexion->prepare($sentencia);//Se prepara esa sentencia SQL y se recoge su valor en esta variable
+            $consulta->execute($valores);//Se ejecuta la sentencia con los datos del array "$valores"
+        } catch (PDOException $e) {//Si salta un error del tipo "PDOException":
+            $log = new Log();//Se crea una instancia de la clase "Log"
+            $mensaje_error = $e->getMessage();//Mensaje de error
+    
+            $log->escribeLog($mensaje_error);//Se ejecuta la función "escribeLog()" a partir del objeto "$log" con el parámetro "$mensaje_error"
+        }
     }
 
     /**
@@ -63,15 +78,22 @@ require "Config.class.php";//Se meten los datos para la conexión a la base de d
      * Descripción: Modifica los datos específicos
      */
     public static function modificarDatos($tabla, $campos_modificaciones, $valores_modificaciones, $campos_condiciones, $tipos_condiciones, $valores_condiciones) {
-        $conexion = self::establecerConexion();//Se ejecuta la función "establecerConexion()" a partir de la clase "DAO" y se recoge su valor de retorno en la variable "$conexion"
-        self::configurarPDO($conexion);//Se ejecuta la función configurarPDO() con el parámetro "$conexion"
-        $modificaciones = self::concatenarModificaciones($campos_modificaciones);//Se ejecuta la función "concatenarCondiciones()" con el parámetro "$campos_modificaciones" y se recoge su valor de retorno en la variable "$modificaciones"
-        $condiciones = self::concatenarCondiciones($campos_condiciones, $tipos_condiciones);//Se ejecuta la función "concatenarCondiciones()" con los parámetros "$campos_condiciones" y "$tipos_condiciones" y se recoge su valor de retorno en la variable "$condiciones"
-        $total_valores = array_merge($valores_modificaciones, $valores_condiciones);//Se junta en un solo array ($total_valores) los valores de las modificaciones y de las condiciones, en ese orden
+        try {//Se prueban los datos siguientes:
+            $conexion = self::establecerConexion();//Se ejecuta la función "establecerConexion()" a partir de la clase "DAO" y se recoge su valor de retorno en la variable "$conexion"
+            self::configurarPDO($conexion);//Se ejecuta la función configurarPDO() con el parámetro "$conexion"
+            $modificaciones = self::concatenarModificaciones($campos_modificaciones);//Se ejecuta la función "concatenarCondiciones()" con el parámetro "$campos_modificaciones" y se recoge su valor de retorno en la variable "$modificaciones"
+            $condiciones = self::concatenarCondiciones($campos_condiciones, $tipos_condiciones);//Se ejecuta la función "concatenarCondiciones()" con los parámetros "$campos_condiciones" y "$tipos_condiciones" y se recoge su valor de retorno en la variable "$condiciones"
+            $total_valores = array_merge($valores_modificaciones, $valores_condiciones);//Se junta en un solo array ($total_valores) los valores de las modificaciones y de las condiciones, en ese orden
 
-        $sentencia = "UPDATE $tabla SET $modificaciones WHERE $condiciones";//Sentencia SQL a preparar
-        $consulta = $conexion->prepare($sentencia);//Se prepara una sentencia SQL y se recoge su valor en esta variable
-        $consulta->execute($total_valores);//Se ejecuta la sentencia con los datos del array "$total_valores"
+            $sentencia = "UPDATE $tabla SET $modificaciones WHERE $condiciones";//Sentencia SQL a preparar
+            $consulta = $conexion->prepare($sentencia);//Se prepara una sentencia SQL y se recoge su valor en esta variable
+            $consulta->execute($total_valores);//Se ejecuta la sentencia con los datos del array "$total_valores"
+        } catch (PDOException $e) {//Si salta un error del tipo "PDOException":
+            $log = new Log();//Se crea una instancia de la clase "Log"
+            $mensaje_error = $e->getMessage();//Mensaje de error
+    
+            $log->escribeLog($mensaje_error);//Se ejecuta la función "escribeLog()" a partir del objeto "$log" con el parámetro "$mensaje_error"
+        }
     }
 
     /**
@@ -79,13 +101,20 @@ require "Config.class.php";//Se meten los datos para la conexión a la base de d
      * Descripción: Borra los datos a partir de la/s condicion/es específica/s
      */
     public static function borrarDatos($tabla, $campos_condiciones, $tipos_condiciones, $valores_condiciones) {
-        $conexion = self::establecerConexion();//Se ejecuta la función "establecerConexion()" a partir de la clase "DAO" y se recoge su valor en la variable "$conexion"
-        self::configurarPDO($conexion);//Se ejecuta la función configurarPDO() con el parámetro "$conexion"
-        $condiciones = self::concatenarCondiciones($campos_condiciones, $tipos_condiciones);//Se ejecuta la función "concatenarCondiciones()" con los parámetros "$campos_condiciones" y "$tipos_condiciones" y se recoge su valor de retorno en la variable "$condiciones"
+        try {//Se prueban los datos siguientes:
+            $conexion = self::establecerConexion();//Se ejecuta la función "establecerConexion()" a partir de la clase "DAO" y se recoge su valor en la variable "$conexion"
+            self::configurarPDO($conexion);//Se ejecuta la función configurarPDO() con el parámetro "$conexion"
+            $condiciones = self::concatenarCondiciones($campos_condiciones, $tipos_condiciones);//Se ejecuta la función "concatenarCondiciones()" con los parámetros "$campos_condiciones" y "$tipos_condiciones" y se recoge su valor de retorno en la variable "$condiciones"
 
-        $sentencia = "DELETE FROM $tabla WHERE $condiciones";//Sentencia SQL a preparar
-        $consulta = $conexion->prepare($sentencia);//Se prepara una sentencia SQL y se recoge su valor en esta variable
-        $consulta->execute($valores_condiciones);//Se ejecuta la sentencia con los datos del array "$valores_condiciones"
+            $sentencia = "DELETE FROM $tabla WHERE $condiciones";//Sentencia SQL a preparar
+            $consulta = $conexion->prepare($sentencia);//Se prepara una sentencia SQL y se recoge su valor en esta variable
+            $consulta->execute($valores_condiciones);//Se ejecuta la sentencia con los datos del array "$valores_condiciones"
+        } catch (PDOException $e) {//Si salta un error del tipo "PDOException":
+            $log = new Log();//Se crea una instancia de la clase "Log"
+            $mensaje_error = $e->getMessage();//Mensaje de error
+
+            $log->escribeLog($mensaje_error);//Se ejecuta la función "escribeLog()" a partir del objeto "$log" con el parámetro "$mensaje_error"
+        }
     }
 
     /**
