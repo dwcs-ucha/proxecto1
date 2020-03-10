a2f5f03fcd3dc178fb14902ea6472bc2728c4f65
luiscorraldc <lcorraldecal@gmail.com>
2020-03-10 20:02:25 +0100

    Modificaciones actividade 6

--- actividades/actividade6/Controlador/sumas.php
+++ actividades/actividade6/Controlador/sumas.php
@@ -9,44 +9,79 @@
 /* * *************************** */
 ?>
 <?php
+
 session_start();
 include_once '../../Modelo/Config.class.php';
 include_once '../../iniciarsmarty.inc.php';
 include_once '../../Modelo/Estatisticas.class.php';
 include_once '../Modelo/Sumas.class.php';
 
+function comprobarcampos($campos) {
+    $errorcampos = false;
+    switch ($dif) {
+        case 'baixa':
+            for ($i = 0; $i < sizeof($campos); $i++) {
+                if (!Validacion::validaNumero_facil($campos[$i])) {
+                    $errorcampos = true;
+                }
+            }
+            break;
+        case 'media':
+            for ($i = 0; $i < sizeof($campos); $i++) {
+                if (!Validacion::validaNumero_medio($campos[$i])) {
+                    $errorcampos = true;
+                }
+            }
+            break;
+        case 'dificil':
+            for ($i = 0; $i < sizeof($campos); $i++) {
+                if (!Validacion::validaNumero_dificil($campos[$i])) {
+                    $errorcampos = true;
+                }
+            }
+            break;
+    }
+    return $errocampos;
+}
+
 $dif = $_SESSION['a6_dif'];
-if(isset($_SESSION['a6_partida'])){
+if (isset($_SESSION['a6_partida'])) {
     $partida = $_SESSION['a6_partida'];
-}else{
+} else {
     $partida = Sumas::crearPartida($dif); //creamos a partida
 }
 $_SESSION['a6_partida'] = $partida;
+$errorcampos = false;
 if (isset($_POST['finalizar'])) {
-    $aciertos = 0;
-    $puntuacion = 0;
-    $resultados = $_POST['res'];
-    for ($i = 0; $i < sizeof($resultados); $i++) {
-        if ($res[$i] == $partida[$i]->resultado) {
-            $aciertos++;
+    if (comprobarcampos($_POST['res'])) {
+        $errorcampos = true;
+    } else {
+        $aciertos = 0;
+        $puntuacion = 0;
+        $resultados = $_POST['res'];
+        for ($i = 0; $i < sizeof($resultados); $i++) {
+            if ($res[$i])
+                if ($res[$i] == $partida[$i]->resultado) {
+                    $aciertos++;
+                }
         }
+        $_SESSION['a6_aciertos'] = $aciertos;
+        switch ($dif) {
+            case 'baixa':
+                $puntuacion = $aciertos;
+                break;
+            case 'media':
+                $puntuacion = $aciertos * 2;
+                break;
+            case 'dificil':
+                $puntuacion = $aciertos * 3;
+                break;
+        }
+        $_SESSION['a6_puntuacion'] = $puntuacion;
+        header("location:resultados.php");
     }
-    $_SESSION['a6_aciertos'] = $aciertos;
-    switch ($dif) {
-        case 'baixa':
-            $puntuacion = $aciertos;
-            break;
-        case 'media':
-            $puntuacion = $aciertos * 2;
-            break;
-        case 'dificil':
-            $puntuacion = $aciertos * 3;
-            break;
-    }
-    $_SESSION['a6_puntuacion'] = $puntuacion;
-    header("location:resultados.php");
 }
-
+$smarty->assign('errorcampos',$errorcampos);
 $smarty->assign('partida', $partida);
 $smarty->assign('dif', $dif);
 $smarty->display('../Vista/sumas.tpl');
