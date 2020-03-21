c56daadb6db0044c499609ff783f2064cedd0137
BreoBeceiro <57591916+BreoArmando@users.noreply.github.com>
2020-03-21 14:06:59 +0100

    Traballando na 'actividade1'...

--- actividades/actividade1/Controlador/acceso.php
+++ actividades/actividade1/Controlador/acceso.php
@@ -1,9 +1,12 @@
 <?php
-    // BreoBeceiro:03/03/2020
+    // BreoBeceiro:21/03/2020
     // PROXECTO 2º AVALIACIÓN | Versión 1.0
 
     include('../Modelo/Xogador.class.php');
     include('../Modelo/DAO.class.php');
+
+    include_once '../../Modelo/Config.class.php'; 
+    include_once Config::$rutaRootPHP.'iniciarsmarty.inc.php';
 
     if(isset($_POST['enviar'])){
         $user= $_POST['nomeXogador'];
@@ -27,62 +30,7 @@
             }
         }
     }
+
+    // PRIMEIRO CARGAS AS CLAVES CO MÉTODO DE SMARTY assign, LOGO AMOSAS A PLANTILLA:
+    $smarty->display("Vista/actividadeDoada.tpl");
 ?>
-<!DOCTYPE html>
-<html lang="gl">
-    <head>
-        <meta charset="UTF-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" />
-        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
-        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
-        <style type="text/css">
-        
-            .col-md-4 { float: right; }
-        
-        </style>
-        <link rel="stylesheet" type="text/css" href="" />
-        <script type="text/javascript">
-
-            // JS
-
-        </script>
-        <title>
-            Rexistro de Puntos
-        </title>
-    </head>
-    <body>
-        <div class="container-fluid">
-            <div class="row">
-                <div class="col-md-4">
-
-                </div>
-                <div class="col-md-4">
-                    <div class="formularioIngreso border rounded">
-                        <form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="POST">
-                            <fieldset>
-                                <legend>Garda o teu resultado</legend>
-                                <div class="form-group">
-                                    <label for="Xogador">Xogador:</label>
-                                    <input type="text" name="nomeXogador" value="<?php isset($xogador)? print $xogador : print "" ?>" id="Xogador" />
-                                </div>
-                                <div class="form-group">
-                                    <label for="Contrasinal">Contrasinal:</label>
-                                    <input type="password" name="contrasinalXogador" id="Contrasinal" />
-                                </div>
-                                <?php isset($camposBaleiros)? print $camposBaleiros : print "" ?>
-                                <?php isset($xogadorNonAtopado)? print $xogadorNonAtopado : print "" ?>
-
-                                <input type="submit" name="enviar" value="Gardar" class="btn btn-primary" />
-                            </fieldset>
-                        </form>
-                    </div>
-                </div>
-                <div class="col-md-4">
-                    
-                </div>
-            </div>
-        </div>
-    </body>
-</html>
