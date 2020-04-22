7189bf825dbe48bd2e4d80a6a38275155e81f255
Santiago <Santiago@Santiago-PC>
2020-04-22 17:38:45 +0200

    Operacións CRUD para xestionar o xogo da actividade 4

--- actividades/actividade4/Controlador/CategoriaController.class.php
+++ actividades/actividade4/Controlador/CategoriaController.class.php
@@ -8,6 +8,7 @@
 include Config::getRutaRootPHP() . 'actividades/actividade4/Modelo/CategoriaVO.class.php';
 
 class CategoriaController {
+
     /**
      * Obtén unha lista aleatoria do tamaño indicado das categorías almacenadas
      * @param int $numCategorias Número de categorías que se queren obter
@@ -19,7 +20,7 @@ class CategoriaController {
         $categorias = array_slice($categorias, 0, $numCategorias); //Obtén un trozo do tamaño indicado do array desordeado
         return $categorias;
     }
-    
+
     /**
      * Obtén unha lista das categorías almacenadas
      * @return array Lista de obxetos CategoriaVO almacenadas
@@ -32,17 +33,16 @@ class CategoriaController {
             $codcategoria = $datosCategoria[1]; //O primeiro campo da consulta correponde á columna 'codcategoria'
             $nome = $datosCategoria[2]; //O segundo campo da consulta correponde á columna 'nome'
             $imaxeprincipal = $datosCategoria[3]; //O terceiro campo da consulta correponde á columna 'imaxeprincipal'
-            
+
             $camposImaxe = ["rutaimaxe"]; //A función do DAO require que todos os campos estén nun array
-            $imaxesCategoria = DAO::leerDatosCondicion("a4_imaxes", $camposImaxe, "a4_categorias_codcategoria",
-                    "=", $codcategoria, PDO::FETCH_COLUMN); //PDO::FETCH_COLUMN fai que devolva o resultado da consulta
-                                                            //nun array unidimensional
+            $imaxesCategoria = DAO::leerDatosCondicion("a4_imaxes", $camposImaxe, "a4_categorias_codcategoria", "=", $codcategoria, PDO::FETCH_COLUMN); //PDO::FETCH_COLUMN fai que devolva o resultado da consulta
+//nun array unidimensional
             $categoria = new Categoria($nome, $imaxeprincipal, $imaxesCategoria);
             $categorias[] = $categoria;
         }
         return $categorias;
     }
-    
+
     /**
      * 
      * @return array Contén os nomes das categorías almacenadas
@@ -55,7 +55,6 @@ class CategoriaController {
         }
         return $nomesCategorias;
     }
-
 
     /**
      * Obtén unha lista de categorías que se corresponden co nomes suministrados
@@ -72,4 +71,113 @@ class CategoriaController {
         }
         return $categoriasSeleccionadas;
     }
+
+    static function borrarCategoria(string $nomeCategoria) {
+        $rutaDirectorioImaxes = Config::getRutaRootPHP() . "/actividades/actividade4/Imaxes/";
+        if (self::getCategoria($nomeCategoria) !== null) {
+            self::borrarDirectorio($rutaDirectorioImaxes . $nomeCategoria);
+            DAO::borrarDatos("a4_categorias", array("nome"), array("="), array($nomeCategoria));
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    private static function borrarDirectorio(string $rutaDirectorio) {
+        if (file_exists($rutaDirectorio)) {
+            $directorio = opendir($rutaDirectorio);
+            while (($arquivoActual = readdir($directorio)) !== false) {
+                if ($arquivoActual !== "." && $arquivoActual !== "..") {
+                    unlink($rutaDirectorio . "/" . $arquivoActual);
+                }
+            }
+            closedir($directorio);
+            rmdir($rutaDirectorio);
+        }
+    }
+
+    static function insertarCategoria(string $nomeCategoria, array $datosImaxePrincipal, array $datosImaxesCategoria) {
+        $rutaDirectorioImaxes = "actividades/actividade4/Imaxes/";
+        $rutaDirectorioCategoria = $rutaDirectorioImaxes . $nomeCategoria . "/";
+        mkdir(Config::getRutaRootPHP() . $rutaDirectorioCategoria);
+        $rutaTemporal = $datosImaxePrincipal["tmp_name"];
+        $nomeImaxePrincipal = $datosImaxePrincipal["name"];
+        $rutaDefinitivaImaxePrincipal = $rutaDirectorioCategoria . $nomeImaxePrincipal;
+        move_uploaded_file($rutaTemporal, Config::getRutaRootPHP() . "/" . $rutaDefinitivaImaxePrincipal);
+
+        for ($i = 0; $i < count($datosImaxesCategoria["name"]); $i++) {
+            $rutaTemporal = $datosImaxesCategoria["tmp_name"][$i];
+            $nomeImaxe = $datosImaxesCategoria["name"][$i];
+            $rutaDefinitiva = $rutaDirectorioCategoria . $nomeImaxe;
+            move_uploaded_file($rutaTemporal, Config::getRutaRootPHP() . $rutaDefinitiva);
+            $rutasImaxesCategoria[] = $rutaDefinitiva;
+        }
+
+        DAO::escribirDatos("a4_categorias", array("codactividade", "nome", "imaxeprincipal")
+                , array("a4", $nomeCategoria, $rutaDefinitivaImaxePrincipal));
+        $codCategoriaInsertada = DAO::leerDatosCondicion("a4_categorias", array("codcategoria"), "nome"
+                        , "=", $nomeCategoria, PDO::FETCH_COLUMN)[0];
+        foreach ($rutasImaxesCategoria as $rutaImaxeCategoria) {
+            DAO::escribirDatos("a4_imaxes", array("rutaimaxe", "a4_categorias_codcategoria")
+                    , array($rutaImaxeCategoria, $codCategoriaInsertada));
+        }
+    }
+
+    static function getCategoria(string $nomeCategoria) {
+        $camposCategoria = ["*"];
+        $datosCategoria = DAO::leerDatosCondicion("a4_categorias", $camposCategoria, "nome", "=", $nomeCategoria);
+        if (!empty($datosCategoria)) { //Compróbase que exista unha categoría con ese nome
+            $datosCategoria = $datosCategoria[0];
+            $codcategoria = $datosCategoria[1]; //O primeiro campo da consulta correponde á columna 'codcategoria'
+            $nome = $datosCategoria[2]; //O segundo campo da consulta correponde á columna 'nome'
+            $imaxeprincipal = $datosCategoria[3]; //O terceiro campo da consulta correponde á columna 'imaxeprincipal'
+
+            $camposImaxe = ["rutaimaxe"]; //A función do DAO require que todos os campos estén nun array
+            $imaxesCategoria = DAO::leerDatosCondicion("a4_imaxes", $camposImaxe, "a4_categorias_codcategoria", "=", $codcategoria, PDO::FETCH_COLUMN); //PDO::FETCH_COLUMN fai que devolva o resultado da consulta
+//nun array unidimensional
+            $categoria = new Categoria($nome, $imaxeprincipal, $imaxesCategoria);
+            return $categoria;
+        } else {
+            return null;
+        }
+    }
+
+    static function borrarImaxeCategoria(string $rutaImaxeEliminar) {
+        unlink(Config::getRutaRootPHP() . $rutaImaxeEliminar);
+        DAO::borrarDatos("a4_imaxes", array("rutaimaxe"), array("="), array($rutaImaxeEliminar));
+    }
+
+    static function actualizarImaxePrincipal(Categoria $categoria, array $datosNovaImaxePrincipal) {
+        $imaxePrincipalAntiga = $categoria->getImaxeCategoria();
+        $rutaDirectorioImaxes = "actividades/actividade4/Imaxes/";
+        $rutaDirectorioCategoria = $rutaDirectorioImaxes . $categoria->getNome() . "/";
+        $rutaTemporal = $datosNovaImaxePrincipal["tmp_name"];
+        $nomeImaxePrincipal = $datosNovaImaxePrincipal["name"];
+        $rutaDefinitivaImaxePrincipal = $rutaDirectorioCategoria . $nomeImaxePrincipal;
+        move_uploaded_file($rutaTemporal, Config::getRutaRootPHP() . "/" . $rutaDefinitivaImaxePrincipal);
+
+        unlink(Config::getRutaRootPHP() . $imaxePrincipalAntiga);
+        DAO::modificarDatos("a4_categorias", array("imaxeprincipal"), array($rutaDefinitivaImaxePrincipal)
+                , array("nome"), array("="), array($categoria->getNome()));
+    }
+
+    static function engadirImaxes(Categoria $categoria, array $datosImaxes) {
+        $rutaDirectorioImaxes = "actividades/actividade4/Imaxes/";
+        $rutaDirectorioCategoria = $rutaDirectorioImaxes . $categoria->getNome() . "/";
+
+        for ($i = 0; $i < count($datosImaxes["name"]); $i++) {
+            $rutaTemporal = $datosImaxes["tmp_name"][$i];
+            $nomeImaxe = $datosImaxes["name"][$i];
+            $rutaDefinitiva = $rutaDirectorioCategoria . $nomeImaxe;
+            move_uploaded_file($rutaTemporal, Config::getRutaRootPHP() . $rutaDefinitiva);
+            $rutasImaxesCategoria[] = $rutaDefinitiva;
+        }
+        $codCategoria = DAO::leerDatosCondicion("a4_categorias", array("codcategoria"), "nome"
+                        , "=", $categoria->getNome(), PDO::FETCH_COLUMN)[0];
+        foreach ($rutasImaxesCategoria as $rutaImaxeCategoria) {
+            DAO::escribirDatos("a4_imaxes", array("rutaimaxe", "a4_categorias_codcategoria")
+                    , array($rutaImaxeCategoria, $codCategoria));
+        }
+    }
+
 }
--- actividades/actividade4/Controlador/xestionActividade.php
+++ actividades/actividade4/Controlador/xestionActividade.php
@@ -3,26 +3,105 @@
 /**
  * @author Santiago Calvo Piñeiro
  */
-include '../../Modelo/Config.class.php';
-include 'Controlador/CategoriaController.class.php';
+include '../../../Modelo/Config.class.php';
+include '../Controlador/CategoriaController.class.php';
 include Config::getRutaRootPHP() . 'Modelo/DAO.class.php';
 include Config::getRutaRootPHP() . 'Modelo/Validacion.class.php';
 include Config::getRutaRootPHP() . 'iniciarsmarty.inc.php';
 include Config::getRutaRootPHP() . 'Modelo/Usuario.class.php';
+include Config::getRutaRootPHP() . 'actividades/actividade4/Modelo/PartidaVO.class.php';
 
 session_start();
 //$usuario = new Usuario("santi", "1234", Usuario::ROL_NORMAL, "2020-03-26", 0);
 //Usuario::insertarNovoUsuario($usuario);
 //Usuario::loginUsuario("santi", "1234");
-//$usuario = Usuario::getUsuarioEnSesion();
-//var_dump($usuario);
-
-$fasePartida = PartidaController::getFasePartida();
-if ($fasePartida !== PartidaVO::FASE_CONFIGURAR) {
-    PartidaController::crearPartidaNova();
+$usuario = Usuario::getUsuarioEnSesion();
+if ($usuario !== null) {
+    if ($usuario->getRol() !== Usuario::ROL_ADMINISTRADOR) {
+        header("Location: ../index.php");
+    }
+} else {
+    header("Location: ../index.php");
 }
 
+if (isset($_POST["nomeCategoriaEliminar"])) {
+    $nomeCategoriaEliminar = $_POST["nomeCategoriaEliminar"];
+    if (CategoriaController::borrarCategoria($nomeCategoriaEliminar) === false) {
+        $mensaxeErro = "Non se puido eliminar a categoría";
+    }
+}
+
+if (isset($_POST["rutaImaxeEliminar"])) {
+    $imaxeEliminar = $_POST["rutaImaxeEliminar"];
+    if (file_exists($imaxeEliminar)) {
+        CategoriaController::borrarImaxeCategoria($imaxeEliminar);
+    } else {
+        $mensaxeErro = "Non existe a imaxe que se quere eliminar";
+    }
+}
+
+if (!empty($_FILES["imaxePrincipal"]["name"]) && isset($_POST["nomeCategoriaMostrar"])) {
+    $nomeCategoriaMostrar = $_POST["nomeCategoriaMostrar"];
+    $categoria = CategoriaController::getCategoria($nomeCategoriaMostrar);
+    $imaxePrincipalNova = $_FILES["imaxePrincipal"];
+    if (Validacion::validaImaxe($imaxePrincipalNova["type"])) {
+        CategoriaController::actualizarImaxePrincipal($categoria, $imaxePrincipalNova);
+    } else {
+        $mensaxeErro = "Imaxe non válida";
+    }
+}
+
+if (!empty($_FILES["imaxesCategoria"]["name"][0]) && isset($_POST["nomeCategoriaMostrar"])) {
+    $nomeCategoriaMostrar = $_POST["nomeCategoriaMostrar"];
+    $categoria = CategoriaController::getCategoria($nomeCategoriaMostrar);
+    $datosImaxes = $_FILES["imaxesCategoria"];
+    if (Validacion::validaImaxes($datosImaxes["type"], 0)) {//O número mínimo é 0 porque se presupón que a categoría xa
+        //ten o número mínimo de imaxes necesarias
+        CategoriaController::engadirImaxes($categoria, $datosImaxes);
+    } else {
+        $mensaxeErro = "As imaxes non son válidas";
+    }
+}
+
+
+if (!empty($_POST["nomeCategoriaMostrar"])) {
+    $nomeCategoriaMostrar = $_POST["nomeCategoriaMostrar"];
+    $categoriaMostrar = CategoriaController::getCategoria($nomeCategoriaMostrar);
+    if ($categoriaMostrar !== null) {
+        $imaxePrincipal = $categoriaMostrar->getImaxeCategoria();
+        $imaxesCategoriaMostrar = $categoriaMostrar->getImaxesXogo();
+    } else { //Se introduce unha categoría que non existe non mostra ningunha categoría
+        header("Location: xestionActividade.php");
+    }
+}
+
+if (isset($_POST["novaCategoria"])) {
+    if ($_POST["novaCategoria"] === "novaCategoria") {
+        $nomeCategoria = $_POST["nomeCategoria"];
+        $imaxePrincipal = $_FILES["imaxePrincipal"];
+        $imaxesCategoria = $_FILES["imaxesCategoria"];
+        if (!Validacion::validaNomeCategoria($nomeCategoria)) {
+            $mensaxeErro = "Nome non válido";
+        } else if (!Validacion::validaImaxe($imaxePrincipal["type"])) {
+            $mensaxeErro = "Imaxe principal non válida";
+        } else if (!Validacion::validaImaxes($imaxesCategoria["type"], PartidaVO::NUMERO_IMAXES_CATEGORIA_MAXIMO)) {
+            $mensaxeErro = "Imaxes da categoría non válidas";
+        } else {
+            CategoriaController::insertarCategoria($nomeCategoria, $imaxePrincipal, $imaxesCategoria);
+        }
+    }
+}
+
+
+$nomesCategoriasAlmacenadas = CategoriaController::getNomesCategoriasAlmacenadas();
 if (isset($mensaxeErro)) {
     $smarty->assign("mensaxeErro", $mensaxeErro);
 }
-$smarty->display("Vista/index.tpl");
+$smarty->assign("nomesCategorias", $nomesCategoriasAlmacenadas);
+if (isset($nomeCategoriaMostrar)) {
+    $smarty->assign("mostrarCategoria", true);
+    $smarty->assign("nomeCategoria", $nomeCategoriaMostrar);
+    $smarty->assign("imaxePrincipal", $imaxePrincipal);
+    $smarty->assign("imaxesCategoriaMostrar", $imaxesCategoriaMostrar);
+}
+$smarty->display("../Vista/xestionActividade.tpl");
