513c393e4dc8373c174f23bff8066beaee660ea8
Santiago <Santiago@Santiago-PC>
2020-05-09 12:43:24 +0200

    Engadida opción de cambiar fonte

--- Controlador/preferencias.php
+++ Controlador/preferencias.php
@@ -5,14 +5,21 @@ include_once Config::$rutaRootPHP . 'iniciarsmarty.inc.php';
 
 if (isset($_POST["preferencias"])) {
     if (isset($_POST["temaOscuro"])) {
-        setcookie("temaOscuro", true, time()+3600, "/");
+        setcookie("temaOscuro", true, time() + 3600, "/");
     } else {
-        setcookie("temaOscuro", true, time()-100, "/");
+        setcookie("temaOscuro", true, time() - 100, "/");
+    }
+    if (isset($_POST["fonteArial"])) {
+        setcookie("fonteArial", true, time() + 3600, "/");
+    } else {
+        setcookie("fonteArial", true, time() - 100, "/");
     }
     header("Location: " . Config::$rutaRootHTML . "Controlador/preferencias.php");
     exit();
 }
 //var_dump($_COOKIE);
 $temaOscuro = isset($_COOKIE["temaOscuro"]) ? "checked" : "";
+$fonteArial = isset($_COOKIE["fonteArial"]) ? "checked" : "";
 $smarty->assign("temaOscuro", $temaOscuro);
-$smarty->display(Config::$rutaRootPHP.'Vista/preferencias.tpl');
+$smarty->assign("fonteArial", $fonteArial);
+$smarty->display(Config::$rutaRootPHP . 'Vista/preferencias.tpl');
--- Vista/layout/head.tpl
+++ Vista/layout/head.tpl
@@ -15,5 +15,8 @@
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
 <link rel="stylesheet" href="{$rutaRootHTML}Vista/estilos/estilos.css" type="text/css">
 {if isset($smarty.cookies.temaOscuro)}
-<link rel="stylesheet" href="{$rutaRootHTML}Vista/estilos/temaOscuro.css" type="text/css">
+    <link rel="stylesheet" href="{$rutaRootHTML}Vista/estilos/temaOscuro.css" type="text/css">
+{/if}
+{if isset($smarty.cookies.fonteArial)}
+    <link rel="stylesheet" href="{$rutaRootHTML}Vista/estilos/fonteArial.css" type="text/css">
 {/if}
